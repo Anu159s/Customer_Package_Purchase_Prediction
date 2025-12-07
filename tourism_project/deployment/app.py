3933b14b27fa63466d8ad501fe73ce47b72a4175
Anu159 <learnandearn159@gmail.com>
2025-12-07 10:35:57 +0000

    Fix Streamlit numeric type errors

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -10,28 +10,96 @@ model = joblib.load(model_path)
 # Streamlit UI for Machine Failure Prediction
 st.title("Customer Package Purchase Prediction App")
 st.write("""
-This application predicts whether a customer will purchase the newly introduced Wellness Tourism Package before contacting them based on different parameters.
+This application predicts whether a customer will purchase the Wellness Tourism Package.
 Please enter the data below to get a prediction.
 """)
 
 # User input
+
 TypeofContact = st.selectbox("Contact Type", ["Self Enquiry", "Company Invited"])
-Age = st.number_input("Age", min_value=18.0, max_value=90.0, value=37.0, step=1)
-CityTier = st.number_input("CityTier", min_value=1.0, max_value=3.0, value=3.0, step=1.0)
-DurationOfPitch = st.number_input("DurationOfPitch", min_value=1, max_value=60, value=15)
-Occupation = st.selectbox("Occupation", ["Salaried", "Free Lancer", "Small Business", "Large Business"])
+
+Age = st.number_input(
+    "Age", min_value=18.0, max_value=90.0, value=35.0, step=1.0
+)
+
+CityTier = st.selectbox("City Tier", [1, 2, 3])
+
+DurationOfPitch = st.number_input(
+    "Duration Of Pitch", min_value=1.0, max_value=60.0, value=15.0, step=1.0
+)
+
+NumberOfPersonVisiting = st.number_input(
+    "Number of Persons Visiting", min_value=1.0, max_value=10.0, value=2.0, step=1.0
+)
+
+NumberOfFollowups = st.number_input(
+    "Number of Followups", min_value=0.0, max_value=10.0, value=2.0, step=1.0
+)
+
+PreferredPropertyStar = st.selectbox(
+    "Preferred Property Star", [1, 2, 3, 4, 5]
+)
+
+NumberOfTrips = st.number_input(
+    "Number of Trips", min_value=0.0, max_value=50.0, value=5.0, step=1.0
+)
+
+PitchSatisfactionScore = st.selectbox(
+    "Pitch Satisfaction Score", [1, 2, 3, 4, 5]
+)
+
+NumberOfChildrenVisiting = st.number_input(
+    "Number of Children Visiting", min_value=0.0, max_value=10.0, value=0.0, step=1.0
+)
+
+MonthlyIncome = st.number_input(
+    "Monthly Income", min_value=1000.0, max_value=1000000.0, value=25000.0, step=1000.0
+)
+
+Occupation = st.selectbox(
+    "Occupation", ["Salaried", "Free Lancer", "Small Business", "Large Business"]
+)
+
 Gender = st.selectbox("Gender", ["Female", "Male", "Fe Male"])
+
+MaritalStatus = st.selectbox(
+    "Marital Status", ["Single", "Married", "Divorced", "Unmarried"]
+)
+
+Designation = st.selectbox(
+    "Designation", ["Executive", "Manager", "Senior Manager", "AVP", "VP"]
+)
+
+ProductPitched = st.selectbox(
+    "Product Pitched",
+    ["Basic", "Standard", "Deluxe", "Super Deluxe", "Premium"]
+)
+
+Passport = st.selectbox("Has Passport?", ["0", "1"])
+OwnCar = st.selectbox("Owns a Car?", ["0", "1"])
+
 
 # Assemble input into DataFrame
 input_data = pd.DataFrame([{
-    'TypeofContact': typeofcontact,
-    'Age': age,
-    'CityTier': citytier,
-    'DurationOfPitch': durationofpitch,
-    'Occupation': occupation,
-    'Gender': gender
-  }])
-
+    "TypeofContact": TypeofContact,
+    "Age": Age,
+    "CityTier": CityTier,
+    "DurationOfPitch": DurationOfPitch,
+    "NumberOfPersonVisiting": NumberOfPersonVisiting,
+    "NumberOfFollowups": NumberOfFollowups,
+    "PreferredPropertyStar": PreferredPropertyStar,
+    "NumberOfTrips": NumberOfTrips,
+    "PitchSatisfactionScore": PitchSatisfactionScore,
+    "NumberOfChildrenVisiting": NumberOfChildrenVisiting,
+    "MonthlyIncome": MonthlyIncome,
+    "Occupation": Occupation,
+    "Gender": Gender,
+    "MaritalStatus": MaritalStatus,
+    "Designation": Designation,
+    "ProductPitched": ProductPitched,
+    "Passport": Passport,
+    "OwnCar": OwnCar
+}])
 
 if st.button("Predict Purchase"):
     prediction = model.predict(input_data)[0]
